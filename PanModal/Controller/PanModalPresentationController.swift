ba99775c56d1139d911994d222741ca7f0ea3c32
Viktoriia Rohozhyna <vrohozhyna@surprisehr.com>
2020-12-24 17:34:06 +0200

    reverse bouncing

--- PanModal/Controller/PanModalPresentationController.swift
+++ PanModal/Controller/PanModalPresentationController.swift
@@ -100,7 +100,6 @@ open class PanModalPresentationController: UIPresentationController {
         return presentedViewController as? PanModalPresentable
     }
 
-    var currentY: CGFloat = .zero
     // MARK: - Views
 
     /**
@@ -182,8 +181,6 @@ open class PanModalPresentationController: UIPresentationController {
         layoutBackgroundView(in: containerView)
         layoutPresentedView(in: containerView)
         configureScrollViewInsets()
-
-        presentable?.panScrollable?.delegate = self
 
         guard let coordinator = presentedViewController.transitionCoordinator else {
             backgroundView.dimState = .max
@@ -672,7 +669,6 @@ private extension PanModalPresentationController {
      Sets the y position of the presentedView & adjusts the backgroundView.
      */
     func adjust(toYPosition yPos: CGFloat) {
-        print("yPos", yPos)
         presentedView.frame.origin.y = max(yPos, presentable?.panScrollable?.isScrolling ?? false ? longFormYPosition : anchoredYPosition)
 
         guard presentedView.frame.origin.y > shortFormYPosition else {
@@ -899,21 +895,6 @@ private extension PanModalPresentationController {
     }
 }
 
-extension PanModalPresentationController: UIScrollViewDelegate {
-
-
-    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
-
-        if scrollView.isBouncingTop {
-            adjust(toYPosition: currentY + (-scrollView.contentOffset.y))
-        } else if scrollView.isBouncingBottom {
-            adjust(toYPosition: currentY + (-scrollView.contentOffset.y))
-        } else {
-            currentY = presentedView.frame.origin.y
-        }
-    }
-}
-
 // MARK: - Helper Extensions
 
 private extension UIScrollView {
@@ -923,32 +904,6 @@ private extension UIScrollView {
      */
     var isScrolling: Bool {
         return isDragging && !isDecelerating || isTracking
-    }
-
-    var isBouncing: Bool {
-        return isBouncingTop || isBouncingBottom
-    }
-
-    var isBouncingTop: Bool {
-        return contentOffset.y < topInsetForBouncing - contentInset.top
-    }
-
-    var isBouncingBottom: Bool {
-        let threshold: CGFloat
-        if contentSize.height > frame.size.height {
-            threshold = (contentSize.height - frame.size.height + contentInset.bottom + bottomInsetForBouncing)
-        } else {
-            threshold = topInsetForBouncing
-        }
-        return contentOffset.y > threshold
-    }
-
-    private var topInsetForBouncing: CGFloat {
-        return safeAreaInsets.top != 0.0 ? -safeAreaInsets.top : 0.0
-    }
-
-    private var bottomInsetForBouncing: CGFloat {
-        return safeAreaInsets.bottom
     }
 }
 
